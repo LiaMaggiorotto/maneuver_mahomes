f91f3fcd30bbbfe3d7d864b191d7218a7f59f2a7
Lia Sexton <liamaggiorotto@gmail.com>
2020-08-31 22:38:20 -0500

    word vomit onto app.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 console.log("It's Britney B*tch");
+
+//create player class
+    // runImg, duckImg, jumpImg,, width, height, x/y location? jumpTimer? grounded? set direction or velocity?
+
+//create let variables :
+    // ex: score, gravity, game speed
+
+// add horizon to canvas
+
+// set keys as event listeners : 
+
+    //.keydown => duckImg / reduced height
+    //.keyup => jumpImg / jumps + use gravity to pull back down
+    // Any key starts the game
+
+// Game start function: includes speed, gravity?, score
+
+// set timer = to score (but reformat to display 10 sec = 100points)
+
+//create jump function - .keyup event listener
+
+//create duck function  - .keydown event listener
+
+// create obstacles array: ex: shuttlecock at 0, western auto at 1 or 2. 
+
+// create a function that causes obstacles to spawn. 
+    // may be able to include math function to increase the rate at which these spawn 
+    // OR game speed increase set to timer intervals
+
+// Game over function: ensure this includes the alert and game is refreshed upon closing the alert
+
+
