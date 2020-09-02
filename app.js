7b4c444312fd79f5ffe8034022eb4d056ff44970
Lia Sexton <liamaggiorotto@gmail.com>
2020-09-01 23:50:26 -0500

    Temp squares added to canvas, unfinished jump function, event listeners and key logic

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// console.log("It's Britney B*tch");
-// console.log($);
+console.log("It's Britney B*tch");
 
 // create const $() variables:
 
@@ -10,53 +9,102 @@ const canvas = document.getElementById("play-canvas");
 // because the game will be in 2D, this allows us to acces the tools of the 2D tool box (paintbrushes and shapes)
 const ctx = canvas.getContext('2d');
 
+// //create let variables :ß
+// // ex: score, gravity, game speed, ground game speed
+
+canvas.width = 800;
+canvas.height = 500;
+
+// let score = O;
+// let 
+
 
 
 
 
 //create player class
+// temp: create blocks:
+
+//Player:
+
+ctx.fillStyle = "#ca2430";
+ctx.fillRect(80, 350, 50, 100);
+
+//Obstacles:
+
+//shuttlecock
+ctx.fillStyle ="#FFB612";
+ctx.fillRect(400, 400, 50, 50);
+
+//sign
+ctx.fillStyle ="#000000";
+ctx.fillRect(700, 325, 50, 50);
+
+
+// To  make them animated, you have to be able to "draw" repeatedly. create class with methods:
+
 // runImg, duckImg, jumpImg,, width, height, x/y location? jumpTimer? grounded? set direction or velocity?
 
 // class Player {
-    //     constructor(){
-        //         this.w = x;
-        //         this.h = h;
-        //         this.x = x;
-        //         this.y = y;
-        //         this.jumpTimer = 
-        //         this.grounded = false;
-        //         this.runningImg =
-        //         this.velocity =
-        //         this.score = 0;
-        //         this.gravity = 
-        //     }
+//         constructor(x, y, w, h){
+//                 this.grounded = false;
+//                 this.x = x;
+//                 this.y = y;
+//                 this.width = x;
+//                 this.height = h;
+//                 this.jumpTimer = 
+//                 this.runningImg =
+//                 this. =
+//                 this.score = 0;
+//                 this.gravity = 
+//             }
+            
+            // method to help find the right side - x-axis location of player
+            // get right () {
+                //     return this.x + this.width;
+                // }
+                // // }
+                
+                
+                //create jump function - .keyup event listener
+
+const mahomesJump = function () {
+    requestAnimationFrame(mahomesJump);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    ctx.beginPath();
+    ctx.fillStyle = "#ca2430";
+    ctx.fillRect(80, 350, 50, 100);
+
+
+}
         
-        // method to help find the right side - x-axis location of player
-        // get right () {
-        //     return this.x + this.width;
-        // }
-        // // }
-        
-        
-        //create let variables :
-        // ex: score, gravity, game speed, ground game speed
-        
-        
-        // add horizon to canvas
+        // add horizon to canvas - can do this in CSS by adding background to canvas.
         
         // set keys as event listeners : 
 
+        window.addEventListener("keydown", mahomesDuck, false);
+        window.addEventListener("keyup", mahomesJump, false);
 
-        // How To Play button pop up:
+        // key input logic
+        let keyUp = false; // case 38
+        let keyDown = false; // case 40
+
+
+
+        // DONE: How To Play button pop up:
 
     const $buttonEl = $('#how-to-play');
     $buttonEl.click(function () {
+        console.log("clickity"); // tests function
         alert("Press any button to start the game. Use the up arrow to jump and the down arrow to help Mahomes dodge under these KC landmarks. The longer you run, the higher your score!");
     });
 
         
         //.keydown => duckImg / reduced height
+
         //.keyup => jumpImg / jumps + use gravity to pull back down
+
+
         // Any key starts the game
         
         // Game start function: includes speed, gravity?, score
@@ -67,7 +115,6 @@ const ctx = canvas.getContext('2d');
         
         // set timer = to score (but reformat to display 10 sec = 100points)
         
-        //create jump function - .keyup event listener
         
         //create duck function  - .keydown event listener
         
@@ -76,6 +123,8 @@ const ctx = canvas.getContext('2d');
         // create a function that causes obstacles to spawn. 
         // may be able to include math function to increase the rate at which these spawn 
         // OR game speed increase set to timer intervals
+
+        // detect hitting obstacles
         
         // Game over function: ensure this includes the alert and game is refreshed upon closing the alert
         
