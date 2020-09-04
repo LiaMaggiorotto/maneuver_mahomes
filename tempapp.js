4fcce330845a61927ca0501fa163e092f5dfd0dd
Lia Sexton <liamaggiorotto@gmail.com>
2020-09-04 13:34:40 -0500

    adjusting mahomes w and h

--- tempapp.js
+++ tempapp.js
@@ -189,7 +189,7 @@ const startGame = function () {
 
 
     // create players using above class:
-    mahomes = new Player(runningMahomes, 80, 250, 225, 225);
+    mahomes = new Player(runningMahomes, 80, 250, 225, 300);
     jumpMahomes = new Player(jumpingMahomes, 80, 250, 225, 225);
     duckMahomes = new Player(duckingMahomes, 80, 250, 186, 150);
 
