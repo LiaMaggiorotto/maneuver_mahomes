92b42fbe0b8c339ef9c869a18988e51530ce4890
Lia Sexton <liamaggiorotto@gmail.com>
2020-11-16 15:39:24 -0600

    updates margin on body for MVP winGame

--- tempapp.js
+++ tempapp.js
@@ -189,7 +189,7 @@ const startGame = function () {
 
 
     // create players using above class:
-    mahomes = new Player(runningMahomes, 80, 250, 200, 325);
+    mahomes = new Player(runningMahomes, 80, 250, 225, 225);
     jumpMahomes = new Player(jumpingMahomes, 80, 250, 225, 225);
     duckMahomes = new Player(duckingMahomes, 80, 250, 186, 150);
 
